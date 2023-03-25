5e5b0a7be3a537d663334c37ee4e27f2f7f9244d
begmuhommet jumayev <begmuhommet.jumayev@gmail.com>
2023-03-25 10:05:19 +0800

    :dizzy: Added loading and favicon

--- src/components/World.tsx
+++ src/components/World.tsx
@@ -1,16 +1,22 @@
-import { OrbitControls } from "@react-three/drei";
+import { Loader, OrbitControls } from "@react-three/drei";
 import WorldScene from "@/components/WorldScene";
 import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
 
 interface IProps {}
 
 const World: React.FC<IProps> = () => {
   return (
-    <Canvas flat camera={{ position: [0, 0, 15] }}>
-      <color args={["#000000"]} attach="background" />
-      <OrbitControls enableZoom={true} />
-      <WorldScene />
-    </Canvas>
+    <>
+      <Canvas flat camera={{ position: [0, 0, 15] }}>
+        <color args={["#000000"]} attach="background" />
+        <OrbitControls enableZoom={true} />
+        <Suspense fallback={null}>
+          <WorldScene />
+        </Suspense>
+      </Canvas>
+      <Loader />
+    </>
   );
 };
 
